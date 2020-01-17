9bbdc01e57ceb7e37f86455b6fab564dc26e2cd7
datapsyche <jithinjkumar@gmail.com>
2020-01-17 16:24:55 +0530

    added flask contents

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 14.1.2020-Flask
-Flask Intro
+# Flask Introduction
+
+-HTTP Verbs - https://www.youtube.com/watch?v=iYM2zFP3Zn0
+
+FLask Reference Videos
+- https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=1235s
+- https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+COMMENTS = []
+
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/comment',  methods=['GET', 'POST'])
+def comment():
+    if request.method == "POST":
+        name = request.form['name']
+        comment = request.form['comment']
+        COMMENTS.append((name, comment))        
+        return render_template('comment.html', COMMENTS=COMMENTS)
+    elif request.method == "GET":
+        return render_template('comment.html')
 
 
 
