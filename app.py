73857ecb6f19903b8ac8708eb0404643b2836732
datapsyche <jithinjkumar@gmail.com>
2020-01-22 15:22:07 +0530

    added dqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request
+# import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///temp.db', echo=True,
+                       connect_args={'check_same_thread': False})
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
 app = Flask(__name__)
+ 
+class Comments(Base):
+    __tablename__ = 'Comments'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    comment_text =Column(String(1000))
+
+    def __repr__(self):
+        return f"{self.name} has commented {self.comment_text}"
+
 
 COMMENTS = []
 
@@ -15,17 +36,22 @@ def about():
 
 @app.route('/comment',  methods=['GET', 'POST'])
 def comment():
-    if request.method == "POST":
+    comment_list = session.query(Comments).all()[::-1]
+    if request.method == "POST":        
         name = request.form['name']
         comment = request.form['comment']
+        obj = Comments(id=len(comment_list)+1, name=name, comment_text=comment)
+        session.add(obj)
+        session.commit()
         COMMENTS.append((name, comment))        
-        return render_template('comment.html', COMMENTS=COMMENTS)
+        return render_template('comment.html', COMMENTS=comment_list)
     elif request.method == "GET":
-        return render_template('comment.html')
+        return render_template('comment.html', COMMENTS=comment_list)
 
 
 
 if __name__ == "__main__":
+    session = Session()
     app.run(debug=True)
 
 
